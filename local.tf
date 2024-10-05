2192bcae38942dd8ed773cb7c791646fef397b98
melvinlee <melvinlee@hotmail.com>
2024-10-05 10:12:21 +0800

    enabled VPC flow log.

--- local.tf
+++ local.tf
@@ -11,8 +11,10 @@ locals {
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
+  s3_bucket_name = "${local.name}-vpc-flow-logs-to-s3"
+
   tags = {
-    Name       = local.name
-    Example    = local.name
+    Name    = local.name
+    Example = local.name
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,43 @@
+# S3 Bucket
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
+
+  bucket        = local.s3_bucket_name
+  policy        = data.aws_iam_policy_document.flow_log_s3.json
+  force_destroy = true
+
+  tags = local.tags
+}
+
+data "aws_iam_policy_document" "flow_log_s3" {
+  statement {
+    sid = "AWSLogDeliveryWrite"
+
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+
+    actions = ["s3:PutObject"]
+
+    resources = ["arn:aws:s3:::${local.s3_bucket_name}/AWSLogs/*"]
+  }
+
+  statement {
+    sid = "AWSLogDeliveryAclCheck"
+
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+
+    actions = ["s3:GetBucketAcl"]
+
+    resources = ["arn:aws:s3:::${local.s3_bucket_name}"]
+  }
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
@@ -11,6 +51,11 @@ module "vpc" {
 
   enable_nat_gateway = true
   single_nat_gateway = true
+
+  enable_flow_log           = true
+  flow_log_destination_type = "s3"
+  flow_log_destination_arn  = module.s3_bucket.s3_bucket_arn
+  flow_log_file_format      = "parquet"
 
   tags = local.tags
 }
