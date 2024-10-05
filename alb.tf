da104e13574a907c798402b71aaaccafb46c24cf
melvinlee <melvinlee@hotmail.com>
2024-10-05 15:11:57 +0800

    refactored code.

--- alb.tf
+++ alb.tf
@@ -93,24 +93,3 @@ module "alb" {
 
   tags = local.tags
 }
-
-module "log_bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "~> 3.0"
-
-  bucket_prefix = "${local.name}-logs-"
-  acl           = "log-delivery-write"
-
-  force_destroy = true
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  attach_elb_log_delivery_policy = true # Required for ALB logs
-  attach_lb_log_delivery_policy  = true # Required for ALB/NLB logs
-
-  attach_deny_insecure_transport_policy = true
-  attach_require_latest_tls_policy      = true
-
-  tags = local.tags
-}
---
+++ s3.tf
@@ -0,0 +1,63 @@
+##################################################################
+# S3 Bucket for VPC Flow Logs and ALB Logs
+##################################################################
+
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
+
+  bucket        = local.s3_bucket_name
+  policy        = data.aws_iam_policy_document.flow_log_s3.json
+  force_destroy = true
+
+  tags = local.tags
+}
+
+data "aws_iam_policy_document" "flow_log_s3" {
+  statement {
+    sid = "AWSLogDeliveryWrite"
+
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+
+    actions = ["s3:PutObject"]
+
+    resources = ["arn:aws:s3:::${local.s3_bucket_name}/AWSLogs/*"]
+  }
+
+  statement {
+    sid = "AWSLogDeliveryAclCheck"
+
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+
+    actions = ["s3:GetBucketAcl"]
+
+    resources = ["arn:aws:s3:::${local.s3_bucket_name}"]
+  }
+}
+
+module "log_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
+
+  bucket_prefix = "${local.name}-logs-"
+  acl           = "log-delivery-write"
+
+  force_destroy = true
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  attach_elb_log_delivery_policy = true # Required for ALB logs
+  attach_lb_log_delivery_policy  = true # Required for ALB/NLB logs
+
+  attach_deny_insecure_transport_policy = true
+  attach_require_latest_tls_policy      = true
+
+  tags = local.tags
+}
